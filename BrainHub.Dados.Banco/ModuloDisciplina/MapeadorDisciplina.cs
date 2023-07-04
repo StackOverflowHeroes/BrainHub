01834fca7ad4c031d343780bcb28d285bfb88225
Leonardo <contato.leoo0@gmail.com>
2023-07-04 20:25:23 -0300

    feat: implementado querys e funções do repositorioDisciplinaEmSql

--- BrainHub.Dados.Banco/ModuloDisciplina/MapeadorDisciplina.cs
+++ BrainHub.Dados.Banco/ModuloDisciplina/MapeadorDisciplina.cs
@@ -7,12 +7,16 @@ namespace BrainHub.Dados.Banco.ModuloDisciplina
     {
         public override void ConfigurarParametros(SqlCommand comando, Disciplina registro)
         {
-            throw new NotImplementedException();
+            comando.Parameters.AddWithValue("ID", registro.id);
+            comando.Parameters.AddWithValue("NOME", registro.nome);
         }
 
         public override Disciplina ConverterRegistro(SqlDataReader leitorRegistros)
         {
-            throw new NotImplementedException();
+            int id = Convert.ToInt32(leitorRegistros["CLIENTE_ID"]);
+            string nome = Convert.ToString(leitorRegistros["CLIENTE_NOME"]);
+
+            return new Disciplina(id, nome);
         }
     }
 }
--- BrainHub.Dados.Banco/ModuloDisciplina/RepositorioDisciplinaEmSql.cs
+++ BrainHub.Dados.Banco/ModuloDisciplina/RepositorioDisciplinaEmSql.cs
@@ -1,18 +1,75 @@
 ﻿
+using BrainHub.Dados.Banco.ModuloMateria;
 using BrainHub.Dominio.ModuloDisciplina;
+using BrainHub.Dominio.ModuloMateria;
 
 namespace BrainHub.Dados.Banco.ModuloDisciplina
 {
     public class RepositorioDisciplinaEmSql : RepositorioSqlBase<Disciplina, MapeadorDisciplina>, IRepositorioDisciplina
     {
-        protected override string sqlInserir => throw new NotImplementedException();
+        protected override string sqlInserir => @"INSERT INTO [TBDisciplina] ( [NOME] ) VALUES ( @NOME );  SELECT SCOPE_IDENTITY();";
 
-        protected override string sqlEditar => throw new NotImplementedException();
+        protected override string sqlEditar => @"UPDATE [TBDisciplina] 
+													SET 
+														[NOME] = @NOME,
+													WHERE 
+														[ID] = @ID";
 
-        protected override string sqlExcluir => throw new NotImplementedException();
+        protected override string sqlExcluir => @"DELETE FROM [TBDisciplina]
+	                                                    WHERE [ID] = @ID;";
 
-        protected override string sqlSelecionarTodos => throw new NotImplementedException();
+        protected override string sqlSelecionarTodos => @"SELECT 
+	                                                        [id] AS DISCIPLINA_ID,
+	                                                        [NOME] AS DISCIPLINA_ID
+	                                                    FROM [TBDisciplina];";
 
-        protected override string sqlSelecionarPorId => throw new NotImplementedException();
+        protected override string sqlSelecionarPorId => @"SELECT 
+															[ID]   AS  DISCIPLINA_ID 
+														   ,[NOME] AS  DISCIPLINA_ID
+														FROM 
+															[TBDisciplina] 
+														WHERE 
+															[ID] = @ID";
+
+        private const string sqlSelecionarMateria = @"SELECT 
+		                                            M.[ID] AS MATERIA_ID,
+		                                            M.[NOME] AS MATERIA_NOME			
+	                                            FROM TBMateria AS M 
+	                                            WHERE M.disciplina_id = @DISCIPLINA_ID;";
+
+        public override Disciplina SelecionarPorId(int id)
+        {
+            Disciplina disciplina = base.SelecionarPorId(id);
+
+            if (disciplina != null)
+                CarregarMaterias(disciplina);
+
+            return disciplina;
+        }
+
+        private void CarregarMaterias(Disciplina disciplina)
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            SqlCommand comandoSelecionarMaterias= conexaoComBanco.CreateCommand();
+            comandoSelecionarMaterias.CommandText = sqlSelecionarMateria;
+
+            comandoSelecionarMaterias.Parameters.AddWithValue("DISCIPLINA_ID", disciplina.id);
+
+            SqlDataReader leitorAluguel = comandoSelecionarMaterias.ExecuteReader();
+
+            MapeadorMateria mapeador = new MapeadorMateria();
+
+            while (leitorAluguel.Read())
+            {
+                Materia materia = mapeador.ConverterRegistro(leitorAluguel);
+
+                disciplina.AdicionarMateria(materia);
+            }
+
+            conexaoComBanco.Close();
+
+        }
     }
 }
