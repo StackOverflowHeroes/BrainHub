af3ddabf11544e72ff8b1e0a8e4d9cf72a5ae0be
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-07-04 19:30:26 -0300

    feat: implementado RepositorioQuestaoEmArquivo e suas configurações em ContextoDados

--- BrainHub.Dados.Arquivo/Compartilhado/ContextoDados.cs
+++ BrainHub.Dados.Arquivo/Compartilhado/ContextoDados.cs
@@ -3,66 +3,71 @@ using System.Text.Json.Serialization;
 
 using BrainHub.Dominio.ModuloDisciplina;
 using BrainHub.Dominio.ModuloMateria;
+using BrainHub.Dominio.ModuloQuestao;
 using BrainHub.Dominio.ModuloTeste;
 
 namespace BrainHub.Dados.Arquivo.Compartilhado
 {
-    public class ContextoDados
-    {
-        private const string CAMINHO_ARQUIVO = "Compartilhado\\BrainHub.json";
+     public class ContextoDados
+     {
+          private const string CAMINHO_ARQUIVO = "Compartilhado\\BrainHub.json";
 
-        public List<Disciplina> disciplinas;
-        public List<Materia> materias;
-        public List<Teste> testes;
-        public ContextoDados()
-        {
-            disciplinas = new List<Disciplina>();
-            materias = new List<Materia>();
-            testes = new List<Teste>();
-        }
+          public List<Disciplina> disciplinas;
+          public List<Materia> materias;
+          public List<Teste> testes;
+          public List<Questao> questoes;
 
-        public ContextoDados(bool carregarDados) : this()
-        {
-            if (carregarDados)
-                CarregarArquivoJson();
-        }
+          public ContextoDados()
+          {
+               disciplinas = new List<Disciplina>();
+               materias = new List<Materia>();
+               testes = new List<Teste>();
+               questoes = new List<Questao>();
+          }
 
-        public void GravarArquivoJson()
-        {
-            JsonSerializerOptions configuracoes = ConfigurarSerializadorJson();
+          public ContextoDados(bool carregarDados) : this()
+          {
+               if (carregarDados)
+                    CarregarArquivoJson();
+          }
 
-            string registrosJson = JsonSerializer.Serialize(this, configuracoes);
+          public void GravarArquivoJson()
+          {
+               JsonSerializerOptions configuracoes = ConfigurarSerializadorJson();
 
-            File.WriteAllText(CAMINHO_ARQUIVO, registrosJson);
-        }
+               string registrosJson = JsonSerializer.Serialize(this, configuracoes);
 
-        private void CarregarArquivoJson()
-        {
-            JsonSerializerOptions configuracoes = ConfigurarSerializadorJson();
+               File.WriteAllText(CAMINHO_ARQUIVO, registrosJson);
+          }
 
-            if (File.Exists(CAMINHO_ARQUIVO))
-            {
-                string registrosJson = File.ReadAllText(CAMINHO_ARQUIVO);
+          private void CarregarArquivoJson()
+          {
+               JsonSerializerOptions configuracoes = ConfigurarSerializadorJson();
 
-                if (registrosJson.Length > 0)
-                {
-                    ContextoDados dadosJson = JsonSerializer.Deserialize<ContextoDados>(registrosJson, configuracoes);
+               if (File.Exists(CAMINHO_ARQUIVO))
+               {
+                    string registrosJson = File.ReadAllText(CAMINHO_ARQUIVO);
 
-                    disciplinas = dadosJson.disciplinas;
-                    materias = dadosJson.materias;
-                    testes = dadosJson.testes;
-                }
-            }
-        }
+                    if (registrosJson.Length > 0)
+                    {
+                         ContextoDados dadosJson = JsonSerializer.Deserialize<ContextoDados>(registrosJson, configuracoes);
 
-        private static JsonSerializerOptions ConfigurarSerializadorJson()
-        {
-            JsonSerializerOptions configuracoes = new JsonSerializerOptions();
-            configuracoes.IncludeFields = true;
-            configuracoes.WriteIndented = true;
-            configuracoes.ReferenceHandler = ReferenceHandler.Preserve;
+                         disciplinas = dadosJson.disciplinas;
+                         materias = dadosJson.materias;
+                         testes = dadosJson.testes;
+                         questoes = dadosJson.questoes;
+                    }
+               }
+          }
 
-            return configuracoes;
-        }
-    }
+          private static JsonSerializerOptions ConfigurarSerializadorJson()
+          {
+               JsonSerializerOptions configuracoes = new JsonSerializerOptions();
+               configuracoes.IncludeFields = true;
+               configuracoes.WriteIndented = true;
+               configuracoes.ReferenceHandler = ReferenceHandler.Preserve;
+
+               return configuracoes;
+          }
+     }
 }
---
+++ BrainHub.Dados.Arquivo/ModuloQuestao/RepositorioQuestaoEmArquivo.cs
@@ -0,0 +1,21 @@
+﻿using BrainHub.Dominio.ModuloQuestao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainHub.Dados.Arquivo.ModuloQuestao
+{
+     public class RepositorioQuestaoEmArquivo : RepositorioArquivoBase<Questao>, IRepositorioQuestao
+     {
+          public RepositorioQuestaoEmArquivo(ContextoDados contexto) : base(contexto)
+          {
+          }
+
+          protected override List<Questao> ObterRegistros()
+          {
+               return contextoDados.questoes;
+          }
+     }
+}
