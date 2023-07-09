9637f84b2584ab5dc96846c9e91343eeb7535abb
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-07-08 21:24:22 -0300

    refactor: alterado configurações Alternativa

--- BrainHub.Dominio/ModuloQuestao/Alternativa.cs
+++ BrainHub.Dominio/ModuloQuestao/Alternativa.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace BrainHub.Dominio.ModuloQuestao
 {
-     public class Alternativa
+     public class Alternativa : EntidadeBase<Alternativa>
      {
+          public Questao questao;
           public string tituloResposta;
           public string letraAlternativa { get; set; }
 
@@ -36,6 +37,19 @@ namespace BrainHub.Dominio.ModuloQuestao
           public override string ToString()
           {
                return $"({letraAlternativa}) - {tituloResposta}";
+          }
+
+          public override void AtualizarRegistros(Alternativa registroAtualizado)
+          {
+               tituloResposta = registroAtualizado.tituloResposta;
+               letraAlternativa = registroAtualizado.letraAlternativa;
+               alternativaCorreta = registroAtualizado.alternativaCorreta;
+               questao = registroAtualizado.questao;
+          }
+
+          public override List<string> ValidarErros()
+          {
+               throw new NotImplementedException();
           }
      }
 }
