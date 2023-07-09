c8b3ef77cedfdcf740bc092c2e56e3d8b445034f
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-07-09 17:21:37 -0300

    feat: adicionado tabelas TBQuestao e TBAlternativa

---
+++ BrainHub.Sql.ProjetoDatabase/dbo/Tables/TBAlternativa.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TBAlternativa] (
+    [id]         INT           IDENTITY (1, 1) NOT NULL,
+    [questao_id] INT           NOT NULL,
+    [titulo]     VARCHAR (MAX) NOT NULL,
+    [letra]      VARCHAR (MAX) NOT NULL,
+    [correta]    BIT           NOT NULL,
+    CONSTRAINT [PK_TBAlternativa] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_TBAlternativa_TBQuestao] FOREIGN KEY ([questao_id]) REFERENCES [dbo].[TBQuestao] ([id])
+);
+
+
---
+++ BrainHub.Sql.ProjetoDatabase/dbo/Tables/TBQuestao.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TBQuestao] (
+    [id]         INT           IDENTITY (1, 1) NOT NULL,
+    [enunciado]  VARCHAR (MAX) NOT NULL,
+    [materia_id] INT           NOT NULL,
+    [resposta]   VARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_TBQuestao] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_TBQuestao_TBMateria] FOREIGN KEY ([materia_id]) REFERENCES [dbo].[TBMateria] ([id])
+);
+
+
