4788a07f12480039ec6df68bfb6f63f492981a61
Viitor Montunes <79266568+Viito404@users.noreply.github.com>
2023-07-08 21:38:11 -0300

    add: adicionado arquivos MapeadorQuestao e RepositorioQuestaoEmSql

---
+++ BrainHub.Dados.Banco/ModuloQuestao/MapeadorQuestao.cs
@@ -0,0 +1,22 @@
+﻿using BrainHub.Dominio.ModuloQuestao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainHub.Dados.Banco.ModuloQuestao
+{
+     public class MapeadorQuestao : MapeadorBase<Questao>
+     {
+          public override void ConfigurarParametros(SqlCommand comando, Questao registro)
+          {
+               throw new NotImplementedException();
+          }
+
+          public override Questao ConverterRegistro(SqlDataReader leitorRegistros)
+          {
+               throw new NotImplementedException();
+          }
+     }
+}
---
+++ BrainHub.Dados.Banco/ModuloQuestao/RepositorioQuestaoEmSql.cs
@@ -0,0 +1,22 @@
+﻿using BrainHub.Dominio.ModuloQuestao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainHub.Dados.Banco.ModuloQuestao
+{
+     public class RepositorioQuestaoEmSql : RepositorioSqlBase<Questao, MapeadorQuestao>
+     {
+          protected override string sqlInserir => @"";
+
+          protected override string sqlEditar => @"";
+
+          protected override string sqlExcluir => @"";
+
+          protected override string sqlSelecionarTodos => @"";
+
+          protected override string sqlSelecionarPorId => @"";
+     }
+}
